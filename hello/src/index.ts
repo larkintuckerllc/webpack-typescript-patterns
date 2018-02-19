41e35c54d9ceab9731dcf01eb562bc6655468d41
John Tucker <john@larkintuckerllc.com>
2018-02-19 07:53:17 -0500

    Fix let

--- hello/src/index.ts
+++ hello/src/index.ts
@@ -11,5 +11,5 @@ interface Person {
 function greeter(person : Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-let user = new Student("Jane", "M.", "User");
+const user = new Student("Jane", "M.", "User");
 document.body.innerHTML = greeter(user);
