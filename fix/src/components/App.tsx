261479432ea5ded8a05643dbb9230f4fb9053d97
John  Tucker <john@larkintuckerllc.com>
2018-02-24 11:15:58 -0500

    Fix

---
+++ fix/src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface AppProps {
+    message: string,
+};
+export default function({ message }: AppProps ) {
+    return <h1>Hello {message}</h1>;
+};
---
+++ fix/src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(
+    <App message="World" />,
+    document.getElementById('root'),
+);
