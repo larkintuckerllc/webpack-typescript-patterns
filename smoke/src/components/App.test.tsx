2de16424ab22c0ff5d928ba53c583658669f2adb
John  Tucker <john@larkintuckerllc.com>
2018-02-25 08:58:47 -0500

    Fixed

--- smoke/src/components/App.test.tsx
+++ smoke/src/components/App.test.tsx
@@ -1,6 +1,6 @@
 /* tslint:disable-next-line  */
 import React from 'react';
-import { render, unmountComponentAtNode }from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
